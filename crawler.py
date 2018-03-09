7ab9e4af6624e2633ac8a1f757141f65845d3c85
Pedrat <pedrat@localhost.localdomain>
2018-03-09 12:11:14 +0000

    asdasd

--- crawler.py
+++ crawler.py
@@ -16,10 +16,11 @@ class threads:
         for x in self.threads:
             x.daemon=True
             x.start()
+
     def startthread(self):
         i=0
         ch.read()
-        for x in range(6500,7700):
+        for x in range(6400,7700):
             i+=1
             if i==100:
                 self.inicia(x,(x+100))
@@ -29,8 +30,8 @@ class threads:
         self.start()
 
 while 1:
-    thr=threads())
-    cprint("Menu\n1-Adicionar pessoas para a pesquisa do ID\n2-Procurar","cyan")
+    thr=threads()
+    cprint("Menu\n1-Adicionar pessoas para a pesquisa do ID\n2-Procurar\n3-Procura calendario por id","cyan")
     opc=input("")
     if opc == "1":
         os.system("clear")
@@ -41,3 +42,50 @@ while 1:
         os.system("clear")
         thr.startthread()
         cprint("Processos a correr em Background!",'red')
+    elif opc == "3":
+        os.system("clear")
+        cprint("Procurar por id o calendario",'cyan')
+        ch.User()
+        '''
+        opcn=input("")
+        while 1:
+            cprint("Qual o dia? (01/01/2018)",'cyan')
+            data = input("")
+            datastrip = data.split("/")
+            lista31=[1,3,5,7,8,10,12]
+            lista30=[4,6,9,11]
+            if (int(datastrip[1]) >= 1) or (int(datastrip[1]) <= 12):
+                if int(datastrip[1]) in lista31:
+                    print("AQUI31")
+                    if (int(datastrip[0]) >= 1) or (int(datastrip[0]) <= 31):
+                        ch.calendar(opcn,data)
+                    else:
+                        cprint("Data invalida!",'red')
+                        continue
+                elif int(datastrip[1]) in lista30:
+                    print("AQUI30")
+                    if (int(datastrip[0]) >= 1) or (int(datastrip[0]) <= 30):
+                        ch.calendar(opcn,data)
+                    else:
+                        cprint("Data invalida!",'red')
+                        continue
+                elif int(datastrip[1]) == 2:
+                    if (int(datastrip[0]) >= 1) or (int(datastrip[0]) <= 28):
+                        ch.calendar(opcn,data)
+                    else:
+                        cprint("Data invalida!",'red')
+                        continue
+                else:
+                    cprint("Data invalida!",'red')
+                    continue
+
+            else:
+                cprint("Data invalida!",'red')
+                continue
+
+        #ch.calendar(opcn,date)
+        '''
+    else:
+        os.system("clear")
+        cprint("Invalido!",'red',attrs=["bold"])
+        continue
--- crawlerheader.py
+++ crawlerheader.py
@@ -1,4 +1,4 @@
-import requests,os,sys
+import requests,os,sys,json,codecs,time
 import threading
 from time import sleep
 from termcolor import colored,cprint
@@ -36,7 +36,7 @@ def thread(num,num2):
             url = "http://trainingserver.atec.pt/TrainingServer/Mulberry/JSON/Controls/Calendar/getCalendarDataSource.ashx?command=_SelectAllSchedulesDataSetGivenByUserId&oId="+num+"&idField=DataValueField&titleField=DataTextField&startDateField=DataStartField&endDateField=DataEndField&backgroundColorField=&textColorField=textcolor&eventColorField=color&description=description&picField=pic&urlField=url&start=1520208000&end=1520812800&_=1520509039082"
             page = requests.get(url)
             page=str(page.content)
-            print(num)
+            #print(num)
             if "como Formador" in page:
                 for x in listaform:
                     if x in page:
@@ -45,11 +45,81 @@ def thread(num,num2):
                         num=int(num)
             else:
                 continue
-            sys.exit(1)
+
         except:
             num=int(num)
             num-=1
             continue
+        sys.exit(1)
+
+def calendar(num,date):
+    lista=[]
+    url = "http://trainingserver.atec.pt/TrainingServer/Mulberry/JSON/Controls/Calendar/getCalendarDataSource.ashx?command=_SelectAllSchedulesDataSetGivenByUserId&oId="+num+"&idField=DataValueField&titleField=DataTextField&startDateField=DataStartField&endDateField=DataEndField&backgroundColorField=&textColorField=textcolor&eventColorField=color&description=description&picField=pic&urlField=url&start=1520208000&end=1520812800&_=1520509039082"
+    page = requests.get(url)
+    jsonconv=json.loads(page.text)
+    #print(jsonconv)
+    for x in jsonconv:
+        #print(x["title"])
+        lista.append(x["title"]+'\n')
+
+    file = codecs.open("teste.html", mode="w",encoding="utf-8-sig")
+    for x in str(jsonconv):
+        #print(x)
+        file.write(x)
+    file.close()
+    os.system("clear")
+    cprint("Concluido.",'red')
+
+def User():
+
+        #fckme=""
+        os.system("clear")
+        #ola = ""
+        x = input("Qual o ID que quer procurar?\n")
+        url = "http://trainingserver.atec.pt/TrainingServer/Mulberry/JSON/Controls/Calendar/getCalendarDataSource.ashx?command=_SelectAllSchedulesDataSetGivenByUserId&oId="+x+"&idField=DataValueField&titleField=DataTextField&startDateField=DataStartField&endDateField=DataEndField&backgroundColorField=&textColorField=textcolor&eventColorField=color&description=description&picField=pic&urlField=url&start=1520208000&end=1520812800&_=1520508461232"
+        contents = requests.get(url)
+        fckme=json.loads(contents.text)
+        lista = []
+        for x in fckme:
+            ola = (str(x["title"])+' Start: '+str(time.ctime(float(x["start"])))+' End: '+str(time.ctime(float(x["end"]))))
+            lista.append(ola)
+        print(lista)
+        while True:
+            os.system("clear")
+            opc = input("1. Procura por Hora\n2. Procura por Dia\n3. Procura por Mês\n4. Procura por Ano\n0. Sair\n")
+            if opc == "1":
+                os.system("clear")
+                opc = input("Hora (00:00:00): ")
+                for x in lista:
+                    if opc in x:
+                        print(x)
+                exit = input("Press any key...")
+            elif opc == "2":
+                os.system("clear")
+                opc = input("Dia (ex: Fri, Sun, Wed): ")
+                for x in lista:
+                    if opc in x:
+                        print(x)
+                exit = input("Press any key...")
+            elif opc == "3":
+                os.system("clear")
+                opc = input("Mês (ex: Jul, Sep, Oct): ")
+                for x in lista:
+                    if opc in x:
+                        print(x)
+                exit = input("Press any key...")
+            elif opc == "4":
+                os.system("clear")
+                opc = input("Ano (ex:2018): ")
+                for x in lista:
+                    if opc in x:
+                        print(x)
+                exit = input("Press any key...")
+            elif opc == "0":
+                break
+            else:
+                print("Insira uma opção valida!")
+                exit = input("Press any key...")
 
 def write(nome):
     file = open("nomesprocurar.txt",'a')
